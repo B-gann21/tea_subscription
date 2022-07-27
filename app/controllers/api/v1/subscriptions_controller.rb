63d44a51bea8f4bef0027e268abd3979ea421dcf
Brylan Gannon <brylan.gannon112@gmail.com>
2022-07-27 11:00:59 -0600

    WIP: add a SubscriptionsController#create method

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    body = JSON.parse(request.body.read, symbolize_names: true)
+    customer = Customer.find(params[:customer_id])
+    tea = Tea.find(body[:tea_id])
+
+    subscription = Subscription.build_from_request(body)
+
+    render json: Api::V1::SubscriptionSerializer.show(subscription)
+  end
+end
